d2203c7f1dcff1b25d94936a1e0e5fef0783add4
studentBorisGans <bgans.ieu2023@student.ie.edu>
2025-09-22 12:20:00 +0200

    extra stuff

--- proxy.py
+++ proxy.py
@@ -16,11 +16,15 @@ class ProxyVideoService:
         Initialize the proxy with a factory that can build the real service.
         Do NOT call the factory here (lazy construction!).
         """
+
         # TODO: store the factory, set the underlying service to None,
         # and initialize an in-memory cache (e.g., a dict).
         self.factory = service_factory
         self.service: Optional[RealVideoService] = None
         self.cache = {}
+        self.MAX_SIZE = 10
+        self.cache_hit = 0
+        self.cache_miss = 0
 
     def _ensure_service(self) -> RealVideoService:
         """Construct the real service on demand (only when needed)."""
@@ -40,9 +44,16 @@ class ProxyVideoService:
         #    store the result in cache, and return it.
 
         if (video_id, quality) in self.cache:
+            self.cache_hit += 1
+            print(f"Cache hit: {self.cache_hit}/{self.cache_hit+self.cache_miss}")
             return self.cache[video_id, quality]
         else:
+            self.cache_miss += 1
             if not self.service:
                 self._ensure_service()
-            self.cache[(video_id, quality)] = self.service.download_compressed(video_id, quality)
-            return self.cache[video_id, quality]
+            compressed_vid = self.service.download_compressed(video_id, quality)
+            if len(self.cache) < self.MAX_SIZE:
+                self.cache[(video_id, quality)] = compressed_vid
+                
+            print(f"Cache miss: {self.cache_hit}/{self.cache_hit+self.cache_miss}")
+            return compressed_vid
